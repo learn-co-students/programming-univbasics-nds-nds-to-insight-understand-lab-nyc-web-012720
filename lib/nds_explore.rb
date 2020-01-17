9f8d52c04350a5e76b4a3006b367eb9dffe98566
Michael Quigley <mikequigley@quigleypublishing.com>
2020-01-17 02:10:57 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,25 +4,59 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  #pp(nds)
-  #nil
+  pp(nds)
+  nil
 end
+
+
 
 def print_first_directors_movie_titles
 #  directors[0][:movies]
+#display_cells
+  movie_index = 0
+  while movie_index < directors_database[0][:movies].count do
+    puts directors_database[0][:movies][movie_index][:title]
+    #puts "movie index is now #{movie_index}."
+    movie_index += 1
+  end
 end
 
-=begin
-value = 50
-puts "Hello"
-puts "Hello"
+#print_first_directors_movie_titles
+#display_cells (directors_database)
+#display_cells (directors_database[0])
+#puts directors_database[0][:movies][0][:title]
 
-def explore (nds)
-  dir = {:name => "name_of_director", :movie => {:title => "movie title!", :thing2 => "second thing", :thing3 => "3rd thing", :year => 1582}}
+def display_cells (array)
+  movie_index = 0
+  while movie_index < array[0][:movies].count do
+    puts array[0][:movies][movie_index][:title]
+    #puts "movie index is now #{movie_index}."
+    movie_index += 1
+  end
 end
 
-puts directors_database
+display_cells (directors_database)
 
-directors_database
+#pp directors_database
+#
+# an AoHoAoH:
+#  the directors_database nds is an array of hashes, containing a hash
+#  with a key ':movies' which points to an array of hashes, which
+#  contains a key ':title' which points to a string.
+#
+#layer_0 - puts directors_database[0].class                      ARRAY
+#layer_1 - puts directors_database[0][:movies].class             HASH
+#layer_2 - puts directors_database[0][:movies][0].class          ARRAY
+#layer_3 - puts directors_database[0][:movies][0][:title].class  HASH
 
-pp(directors_database)
+#  "Jaws" is the 1st movie by the 1st director.
+#  "Jaws" is the value of the key ':title', for the first element
+#  in the array (movie_index) pointed to by the ':movies' key,
+#  for the first director in the directors_database.
+#
+#
+
+
+#def explore (nds)
+#  dir = {:name => "name_of_director", :movie => {:title => "movie title!", :thing2 => "second thing", :thing3 => "3rd thing", :year => 1582}}
+#end
